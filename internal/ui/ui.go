67dee2bb41a30b3fdea9c60877eb8d46187505a6
mnsdojo <manishkumar97430@gmail.com>
2024-09-08 22:27:27 -0400

    added ui utils

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -1,3 +1,31 @@
-package ui 
+package ui
 
+import (
+	"fmt"
 
+	"github.com/gookit/color"
+)
+
+var icons = map[string]string{
+	"OS":      "🍎",
+	"Machine": "□",
+	"Kernel":  "⚙",
+	"Uptime":  "◷",
+	"Shell":   "⌘",
+	"CPU":     "⏣",
+	"Memory":  "▤",
+}
+
+func PrintInfo(label, value string, labelColor color.Color) {
+	icon := icons[label]
+	labelColor.Printf("%-2s %-10s", icon, label)
+	fmt.Printf(" →  %s\n", value)
+}
+
+func PrintColorDots() {
+	colors := []color.Color{color.FgWhite, color.FgWhite, color.FgBlue, color.FgMagenta, color.FgBlue, color.FgYellow, color.FgGreen, color.FgRed}
+	for _, c := range colors {
+		c.Print("● ")
+	}
+	fmt.Println()
+}
